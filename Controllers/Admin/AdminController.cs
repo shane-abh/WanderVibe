bbc05d2683836166f8bb3be371a90187266a5a4a
ankitbadani04 <ankitbadani04@gmail.com>
2024-08-09 15:41:23 -0400

    Design dashboard.

--- Controllers/Admin/AdminController.cs
+++ Controllers/Admin/AdminController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WanderVibe.Models;
 
 namespace WanderVibe.Controllers.Admin
 {
@@ -8,11 +10,32 @@ namespace WanderVibe.Controllers.Admin
     [Route("admin/")]
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
-    {        
+    {
+        private readonly TravelDbContext _context;
+
+        public AdminController(TravelDbContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet("")]
         public IActionResult Dashboard()
         {
-            return View();
+            ViewBag.TotalUsers = _context.Users.Count();
+            ViewBag.TotalPackages = _context.TravelPackages.Count();
+            ViewBag.TotalBookings = _context.Bookings.Count();
+            ViewBag.TotalHotels = _context.Hotels.Count();
+            ViewBag.TotalFlights = _context.Flights.Count();
+            ViewBag.TotalService = _context.Services.Count();
+
+            var recentBookings = _context.Bookings
+                                 .Include(b => b.User)
+                                 .Include(b => b.TravelPackage)
+                                 .OrderByDescending(b => b.BookingDate)
+                                 .Take(7)
+                                 .ToList();
+
+            return View("Dashboard", recentBookings);
         }
     }
 }
